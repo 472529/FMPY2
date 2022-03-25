e7e69eb3a5ca28d0f26b57954fdb896092aa29f3
472529@escg.ac.uk <472529@escg.ac.uk>
2022-03-25 10:29:45 +0000

    enemy movement done

--- FMPY2/Assets/Scripts/EnemyMovement.cs
+++ FMPY2/Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform target;
+    [SerializeField] float rotationalDamp = 0.5f;
+    [SerializeField] float moveSpeed = 10f;
+    private void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void Update()
+    {
+        Turn();
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    private void Turn()
     {
-        
+        Vector3 pos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(pos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        transform.position += transform.forward* moveSpeed * Time.deltaTime;
     }
 }
